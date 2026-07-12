8aa283b1a72ee81a87a2284baa23611eaba95382
olisa ogboh <ogboholisa@gmail.com>
2026-07-12 20:49:58 +0100

    css change

--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -207,7 +207,7 @@ export default function LoginPage() {
           minHeight: "100vh",
           display: "flex",
           alignItems: "center",
-          paddingTop: "30px",
+          paddingTop: "0",
         }}
       >
         <div
