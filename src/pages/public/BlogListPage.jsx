4ed06cf7f9482f2e127a9213bf63e97804dbc6f2
olisa ogboh <ogboholisa@gmail.com>
2026-07-22 09:20:33 +0100

    css fix

--- src/pages/public/BlogListPage.jsx
+++ src/pages/public/BlogListPage.jsx
@@ -487,7 +487,7 @@ export default function BlogListPage() {
         </Container>
       </section>
 
-      <Container className="premium-container">
+      <Container>
         <NewsletterSection />
       </Container>
     </main>
